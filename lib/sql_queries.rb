d0ddf201e154bcbb8b3fc6af8f85c034e516f912
lieby weintraub <liebahinda@gmail.com>
2019-04-25 18:50:43 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,8 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT project.title, SUM(pledge.amount)
 FROM PROJECTS 
-INNER JOIN pledges "
+INNER JOIN pledges 
+ON project.id=pledges.id "
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
