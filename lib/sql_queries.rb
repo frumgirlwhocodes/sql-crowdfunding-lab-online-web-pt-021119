2535e90a6f7714a6bd6a8c58acc654a2a4060fc4
lieby weintraub <liebahinda@gmail.com>
2019-04-25 19:15:43 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -49,5 +49,8 @@ JOIN pledges
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"SELECT projects.category, SUM("
+"SELECT projects.category, SUM(pledges.amount)
+FROM projects 
+INNER JOIN pledges 
+"
 end
