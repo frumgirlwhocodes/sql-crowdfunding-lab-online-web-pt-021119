632515f1a858c58fe4bdc5158b0e4ed3cbc9e12e
lieby weintraub <liebahinda@gmail.com>
2019-04-25 19:00:18 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,7 +11,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 FROM PROJECTS 
 INNER JOIN pledges 
 ON project.id=pledged.project_id
-ORDER BY project.title"
+ORDER BY project.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
